c8997562f9cab4232c3bcab8b7e2598e7e4ca681
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-14 09:27:09 -0500

    Updated Temporal

--- service/web/vite.config.ts
+++ service/web/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    build: {
+        // Adjust chunk size warning limit (in kbs). Default = 500 kbs
+        chunkSizeWarningLimit: 1024
+    },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // Absolute Imports using React with Ts and Vite. Check tsconfig.json
+        }
+    }
 })
