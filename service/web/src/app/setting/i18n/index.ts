c9754305c67d62bb966f756604876a9f5a7ab05c
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-15 10:45:48 -0500

    Updated Temporal

--- service/web/src/app/setting/i18n/index.ts
+++ service/web/src/app/setting/i18n/index.ts
@@ -1,3 +1,6 @@
 import { i18n } from './i18n.ts'
+import * as i18nType from './i18n.type.ts'
 
 export { i18n }
+
+export type { i18nType }
--- service/web/src/app/setting/route/index.ts
+++ service/web/src/app/setting/route/index.ts
@@ -1,3 +1,6 @@
 import { route } from './route.ts'
+import * as routeType from './route.type.ts'
 
 export { route }
+
+export type { routeType }
