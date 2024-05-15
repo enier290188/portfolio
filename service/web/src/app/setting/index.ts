9ff9205173a3afbd650e62c10c19ce12136a6eaa
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-15 12:41:05 -0500

    Updated Temporal

--- service/web/src/app/setting/index.ts
+++ service/web/src/app/setting/index.ts
@@ -1,9 +1,15 @@
+import { alert } from './alert'
+import { auth } from './auth'
 import { i18n } from './i18n'
+import { online } from './online'
 import { route } from './route'
 import { theme } from './theme'
 
 export const setting = {
+    alert,
+    auth,
     i18n,
+    online,
     route,
     theme,
 }
