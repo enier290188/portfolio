f44015602fa05792c1df91e68b40a21f3515262e
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-19 16:33:59 -0500

    Updated Temporal

--- service/api/app/api/v1/workspace/root/user/route.py
+++ service/api/app/api/v1/workspace/root/user/route.py
@@ -45,7 +45,7 @@ async def create(request: api_schema.UserRequestCreate, auth_response: auth_depe
     data_email: str = data.get('email', '')
     user_orm = await user_service.get_by_email(db_async_session, data_email)
     if user_orm is not None:
-        raise user_exception.Http409Email
+        raise user_exception.Http409EmailAlreadyExists
 
     data_company_id: str = data.get('company_id', '')
     company_orm = await company_service.get_by_id(db_async_session, data_company_id)
