8f004f37860759d82de3ada4d305b2aff7ef9dd4
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-22 14:23:55 -0500

    service-web: Updated `Security`

--- service/web/src/app/security/Security.tsx
+++ service/web/src/app/security/Security.tsx
@@ -8,30 +8,43 @@ export const Security = ({ children }: { children?: appType.TypeChildrenProps })
 
     const contextAccessToken = React.useContext(app.context.accessToken.Context)
     const accessToken = contextAccessToken.getValue()
-    const accessTokenActionUpdateValue = contextAccessToken.updateValue
+    const accessTokenActionRemoveValue = contextAccessToken.removeValue
+
+    const contextUser = React.useContext(app.context.user.Context)
+    const user = contextUser.getUser()
+    const userActionRemoveUser = contextUser.removeUser
+    const userActionSyncUp = contextUser.syncUp
 
     const authenticate = React.useCallback(async () => {
         console.log('')
         console.log('********** ********** ********** ********** **********')
         console.log('>>> authenticate')
+        console.log(interval?.date)
 
-        if (accessToken) {
+        if (interval?.date && accessToken && user?.id) {
             console.log(accessToken)
 
             const response = await app.service.account.index(accessToken)
             console.log(response)
 
             if (response.status === 200) {
-                accessTokenActionUpdateValue(response.data.auth.access_token)
+                userActionSyncUp(response.data.auth.user)
+                intervalActionStart(5000)
+            } else {
+                if ('error' in response.data.detail) {
+                    accessTokenActionRemoveValue()
+                    userActionRemoveUser()
+                    intervalActionStop()
+                } else {
+                    intervalActionStart(10000)
+                }
             }
-
-            intervalActionStart(1000)
         } else {
             intervalActionStop()
         }
-    }, [intervalActionStart, intervalActionStop, accessToken, accessTokenActionUpdateValue])
+    }, [interval?.date, intervalActionStart, intervalActionStop, accessToken, accessTokenActionRemoveValue, user?.id, userActionRemoveUser, userActionSyncUp])
 
-    React.useEffect(() => {
+    React.useLayoutEffect(() => {
         authenticate()
             .then(() => null)
             .catch(() => null)
