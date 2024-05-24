a0b620b048fb6dbfa4f4ea00dac9e6be3e686c7c
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-24 08:02:01 -0500

    Updated Structure

--- service/api/app/api/v1/sync/route.py
+++ service/api/app/api/v1/sync/route.py
@@ -1,5 +1,13 @@
 from fastapi.routing import APIRouter
 
+from app.api.v1.sync.account import (
+    route as account_route,
+)
+
 router = APIRouter(
     prefix='/sync',
+)
+
+router.include_router(
+    account_route.router,
 )
