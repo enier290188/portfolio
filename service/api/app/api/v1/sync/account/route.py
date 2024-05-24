3ac509c27301ebbc8b2d8369b3170c5d63f0cdad
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-24 08:05:32 -0500

    Updated Structure

--- service/api/app/api/v1/sync/account/route.py
+++ service/api/app/api/v1/sync/account/route.py
@@ -1,22 +1,10 @@
-from typing import Annotated
-
-from fastapi import status
-from fastapi.param_functions import Depends
-from fastapi.responses import Response
 from fastapi.routing import APIRouter
-from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
-from app.api.v1.page.account import (
+from app.api.v1.sync.account import (
     schema as api_schema,
 )
 from app.module.auth import (
     dependency as auth_dependency,
-    exception as auth_exception,
-    jwt as auth_jwt,
-    service as auth_service,
-)
-from app.module.db import (
-    dependency as db_dependency,
 )
 
 router = APIRouter(
@@ -25,81 +13,8 @@ router = APIRouter(
 )
 
 
-@router.post(path='/login/', status_code=status.HTTP_200_OK, response_model=api_schema.LoginResponse)
-async def login(response: Response, request: Annotated[OAuth2PasswordRequestForm, Depends()], db_async_session: db_dependency.DependDBAsyncSession):
-    username: str = request.username
-    password: str = request.password
-
-    user_orm = await auth_service.get_user_by_email(db_async_session, username)
-    if user_orm is None:
-        raise auth_exception.Http401IncorrectUsernameOrPassword
-    if not auth_service.verify_user_password_plain(password, user_orm.password):
-        raise auth_exception.Http401IncorrectUsernameOrPassword
-    if not user_orm.is_active:
-        raise auth_exception.Http401UserInactive
-    if user_orm.has_permission_of_root is False and user_orm.has_permission_of_admin is False and user_orm.has_permission_of_sale is False and user_orm.has_permission_of_project is False:
-        raise auth_exception.Http401UserMustBelongToAGroup
-    if user_orm.company_id is None and not user_orm.has_permission_of_root:
-        raise auth_exception.Http401UserMustBelongToACompany
-
-    sub: str = str(user_orm.id)
-
-    access_token = auth_jwt.create_access_token(sub=sub)
-    response.headers['Authorization'] = 'Bearer {access_token}'.format(access_token=access_token)
-
-    return api_schema.LoginResponse(
-        access_token=access_token,
-    )
-
-
-@router.post(path='/login-sync/', response_model=api_schema.LoginSyncResponse)
-async def login_sync(auth_response: auth_dependency.DependAuth):
-    return api_schema.LoginSyncResponse(
-        **dict(auth_response)
-    )
-
-
 @router.post(path='/sync/', response_model=api_schema.SyncResponse)
 async def sync(auth_response: auth_dependency.DependAuth):
     return api_schema.SyncResponse(
         **dict(auth_response)
-    )
-
-
-@router.get(path='/profile/{id}/', response_model=api_schema.ProfileItemResponse)
-async def profile_get(auth_response: auth_dependency.DependAuth, db_async_session: db_dependency.DependDBAsyncSession):
-    data_user_id: str = auth_response.auth.user.id
-    user_orm = await auth_service.get_user_by_id(db_async_session, data_user_id)
-    if user_orm is None:
-        raise auth_exception.Http404UserNotFound
-
-    return api_schema.ProfileItemResponse(
-        **dict(auth_response),
-        item=api_schema.ProfileResponse(**dict(user_orm.__dict__)),
-    )
-
-
-@router.patch(path='/profile/{id}/info/', response_model=api_schema.ProfileItemResponse)
-async def profile_info_update(auth_response: auth_dependency.DependAuth, db_async_session: db_dependency.DependDBAsyncSession):
-    data_user_id: str = auth_response.auth.user.id
-    user_orm = await auth_service.get_user_by_id(db_async_session, data_user_id)
-    if user_orm is None:
-        raise auth_exception.Http404UserNotFound
-
-    return api_schema.ProfileItemResponse(
-        **dict(auth_response),
-        item=api_schema.ProfileResponse(**dict(user_orm.__dict__)),
-    )
-
-
-@router.patch(path='/profile/{id}/password/', response_model=api_schema.ProfileItemResponse)
-async def profile_info_update(auth_response: auth_dependency.DependAuth, db_async_session: db_dependency.DependDBAsyncSession):
-    data_user_id: str = auth_response.auth.user.id
-    user_orm = await auth_service.get_user_by_id(db_async_session, data_user_id)
-    if user_orm is None:
-        raise auth_exception.Http404UserNotFound
-
-    return api_schema.ProfileItemResponse(
-        **dict(auth_response),
-        item=api_schema.ProfileResponse(**dict(user_orm.__dict__)),
     )
--- service/api/app/api/v1/sync/account/schema.py
+++ service/api/app/api/v1/sync/account/schema.py
@@ -1,42 +1,7 @@
-from datetime import datetime
-from typing import Annotated
-
-from pydantic.fields import Field
-from pydantic.main import BaseModel
-from pydantic.types import UUID4
-
 from app.module.auth import (
     schema as auth_schema,
 )
-from app.module.user import (
-    schema as user_schema,
-)
-
-
-class LoginResponse(BaseModel):
-    access_token: str
-
-
-class LoginSyncResponse(auth_schema.AuthResponse):
-    pass
 
 
 class SyncResponse(auth_schema.AuthResponse):
     pass
-
-
-class ProfileResponse(user_schema.UserResponse):
-    created_at: Annotated[datetime, Field(exclude=True)]
-    updated_at: Annotated[datetime, Field(exclude=True)]
-
-    is_active: Annotated[bool, Field(exclude=True)]
-    has_permission_of_root: Annotated[bool, Field(exclude=True)]
-    has_permission_of_admin: Annotated[bool, Field(exclude=True)]
-    has_permission_of_sale: Annotated[bool, Field(exclude=True)]
-    has_permission_of_project: Annotated[bool, Field(exclude=True)]
-
-    company_id: Annotated[UUID4 | None, Field(exclude=True)]
-
-
-class ProfileItemResponse(auth_schema.AuthResponse):
-    item: ProfileResponse
