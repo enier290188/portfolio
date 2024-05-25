be7e94edf11c339d055447045f4dcba94849764c
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-25 16:23:01 -0500

    service-web: Update Temporal

--- service/web/src/app/page/account/forgot/Forgot.tsx
+++ service/web/src/app/page/account/forgot/Forgot.tsx
@@ -1,7 +1,32 @@
 import { app } from '@./app'
+import { mui } from '@./package/material-ui'
 import { router } from '@./package/react-router'
+import React from 'react'
 
-const View = () => <>View Forgot</>
+const View = () => {
+    const contextI18n = React.useContext(app.context.i18n.Context)
+    const i18nLanguage = contextI18n.getLanguage()
+    const i18n = React.useMemo(() => app.setting.i18n.getNode(app.setting.i18n.app.page.account.forgot, i18nLanguage), [i18nLanguage])
+
+    return (
+        <app.layout.main.component.structure.page.Page key={'send-code'} maxWidth={'375px'}>
+            <app.layout.main.component.structure.head.Head>
+                <app.layout.main.component.structure.box.title.Title level={1}>
+                    <mui.icon.PersonSearch />
+                    {i18n.getText('send-code.title')}
+                </app.layout.main.component.structure.box.title.Title>
+            </app.layout.main.component.structure.head.Head>
+            <app.component.divider.Divider />
+            <app.layout.main.component.structure.body.Body>
+                <mui.component.Box component={'form'} width={'100%'} noValidate={true} autoComplete={'off'} onSubmit={(event) => event.preventDefault()}>
+                    <app.layout.main.component.structure.box.content.Content>
+                        <app.component.underConstruction.UnderConstruction />
+                    </app.layout.main.component.structure.box.content.Content>
+                </mui.component.Box>
+            </app.layout.main.component.structure.body.Body>
+        </app.layout.main.component.structure.page.Page>
+    )
+}
 
 export const Forgot = () => {
     return (
