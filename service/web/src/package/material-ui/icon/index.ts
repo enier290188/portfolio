f4a3cf762e2199add1513dc84fc94c48aae84088
Enier Ramos Garcia <enier290188@gmail.com>
2024-05-27 14:27:45 -0500

    service-web: Update package `material-ui`

--- service/web/src/package/material-ui/icon/index.ts
+++ service/web/src/package/material-ui/icon/index.ts
@@ -1,4 +1,4 @@
-import { AccountCircle, AddAPhoto, AddCircle, ArrowCircleDown, ArrowCircleUp, Business, BusinessCenter, Close, CloudSync, Crop, Dashboard, DeleteForever, Description, DoneOutline, Edit, Email, Error, Group, Image, ImportantDevices, Key, KeyboardArrowDown, KeyboardArrowRight, Language, LockReset, Login, Logout, ManageAccounts, Mediation, Notifications, NotificationsNone, Password, People, PersonPin, PersonSearch, Phone, PhotoCamera, Restore, Save, Search, Send, Settings, Sort, Task, Translate, Update, ViewCarousel, Visibility, VisibilityOff, Widgets } from '@mui/icons-material'
+import { AccountCircle, AddAPhoto, AddCircle, ArrowCircleDown, ArrowCircleUp, Business, BusinessCenter, Close, CloudSync, Crop, Dashboard, DeleteForever, Description, DoneOutline, Edit, Email, Error, Group, Image, ImportantDevices, Key, KeyboardArrowDown, KeyboardArrowRight, Language, LockReset, Login, Logout, ManageAccounts, Mediation, Notifications, NotificationsNone, Password, People, PersonPin, PersonSearch, Phone, PhotoCamera, RadioButtonChecked, RadioButtonUnchecked, Restore, Save, Search, Send, Settings, Sort, Task, Translate, Update, ViewCarousel, Visibility, VisibilityOff, Widgets } from '@mui/icons-material'
 
 export const icon = {
     AccountCircle,
@@ -38,6 +38,8 @@ export const icon = {
     Key,
     KeyboardArrowDown,
     KeyboardArrowRight,
+    RadioButtonChecked,
+    RadioButtonUnchecked,
     Restore,
     Save,
     Search,
