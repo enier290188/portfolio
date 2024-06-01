3e741b81033d97939c25b124fc737fd48de14f02
Enier Ramos Garcia <enier290188@gmail.com>
2024-06-01 14:10:37 -0500

    service-web: Updated component `checkbox`

--- service/web/src/app/component/field/checkbox/Checkbox.tsx
+++ service/web/src/app/component/field/checkbox/Checkbox.tsx
@@ -42,6 +42,7 @@ export const Checkbox = ({ required = true, label = '', color = 'primary', size 
                 control={
                     <mui.component.Checkbox
                         /* */
+                        {...field}
                         icon={<mui.icon.RadioButtonUnchecked />}
                         checkedIcon={<mui.icon.RadioButtonChecked />}
                         required={required}
@@ -51,7 +52,6 @@ export const Checkbox = ({ required = true, label = '', color = 'primary', size 
                         disableRipple={false}
                         autoFocus={autoFocus}
                         {...checkboxPropsRest}
-                        {...field}
                     />
                 }
                 sx={{
